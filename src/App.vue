08bb722a97e48d02f0f1f5db08e3dbcdb1924c43
hyeonah <hyeonah.hello@gmail.com>
2022-06-21 16:31:25 +0900

    config: Set up vue project with webpack

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <!-- Remove & use example component! -->
+  <HelloWorld />
+</template>
+
+<script>
+import HelloWorld from '~/components/HelloWorld'
+
+export default {
+  components: {
+    HelloWorld
+  }
+}
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<template>
+  <h2 @click="changeMessage">
+    {{ msg }}
+  </h2>
+</template>
+
+<script>
+export default {
+  computed: {
+    msg() {
+      return this.$store.state.message.msg
+    }
+  },
+  methods: {
+    changeMessage() {
+      this.$store.dispatch('message/changeMessage', 'Hello Vue!!!')
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  h2 {
+    font-size: 22px;
+    & + img {
+      width: 120px;
+    }
+  }
+</style>
