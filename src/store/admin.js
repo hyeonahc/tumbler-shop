81a40ef693a5de39cd275beb2d9e170f0b74c06c
Kimchaewook <allurings2@naver.com>
2022-07-08 17:40:19 +0900

    feat: Add CommonAdminAPI, DeleteProduct, EditProduct

--- src/store/admin.js
+++ src/store/admin.js
@@ -2,6 +2,7 @@ export default {
   namespaced : true,
   state: () => ({
     allProducts: [],
+    salesDetails: [],
   }),
 
   getters: {
@@ -9,68 +10,96 @@ export default {
   },
 
   mutations : {
+    assignState(state, payload) {
+      Object.keys(payload).forEach(key => {
+        state[key] = payload[key]
+      })
+    },
     addState(state, payload) {
       state.allProducts.push(payload)
-      console.log(state)
+    },
+    delete(state, payload) {
+      const all = state.allProducts.findIndex(product => product.id === payload)
+      state.allProducts.splice(all, 1)
+    },
+    updata(state, payload) {
+      state.type = payload
     }
   },
 
   actions: {
-    
     // 모든 제품 조회
-
-    async productsInquiry(products, payload = {}) {
-      const { commit } = products
-      const {id, title, price, description, tags, thumbnail, isSildOut} = payload
-
-      const { allProducts } = await request({
+    async allProductsLookup({ commit }) {
+      const res = await request({
         url: 'products',
         method: 'GET',
-        body: {
-          id,
-          title,
-          price,
-          description,
-          tags,
-          thumbnail,
-          isSildOut
-        }
       })
-        commit('addState', allProducts)
+        commit('assignState',{allProducts: res})
         console.log('모든 제품 조회')
     },
 
     // 제품 추가
     async addProduct({ commit }, payload = {}) {
-      
       const res = await request({
         url: 'products',
         method: 'POST',
-        data: {
+        body: {
          ...payload
         }
       })
-      commit('addState', res)
-      console.log(res)
-    
+      commit('assignState', {allProducts: res})
+      console.log('제품 추가')
     },
-  },
 
+    // 제품 삭제
+    async deleteProduct({ commit },id) {
+      const res = await commonRequst({
+        url:`${id.id}`,
+        method: 'DELETE',
+      })
+        commit('delete', res)
+        console.log('제품 삭제')
+    },
+
+    // 제품 수정
+    async editProduct({ commit }, id) {
+      const res = await commonRequst({
+        url:`${id.id}`,
+        method: 'PUT',
+      })
+        commit('updata', res)
+        console.log(res)
+    },
+
+  },
 }
 
-
 async function request(options) {
-  // const accessToken = window.localStorage.getItem('token')
-  const {url = '', method, data } = options
+  const {url = '', method, body } = options
   const res = await fetch(`https://asia-northeast3-heropy-api.cloudfunctions.net/api/${url}`, {
     method,
     headers: {
       'content-type': 'application/json',
       'apikey': 'FcKdtJs202204',
       'username': 'TEAM_1',
-      'masterKey': true,
+      masterKey: true,
     },
-    data: JSON.stringify(data)
+    body: JSON.stringify(body)
   })
-  return res.json()
+    return await res.json()
+}
+
+// 공용 관리자
+
+async function commonRequst(id) {
+  const {url = '', method} = id
+  await fetch(`https://asia-northeast3-heropy-api.cloudfunctions.net/api/products/${url}`, {
+    method,
+    headers: {
+      'content-type': 'application/json',
+      'apikey': 'FcKdtJs202204',
+      'username': 'TEAM_1',
+      masterKey: true,
+    },
+   })
 }
