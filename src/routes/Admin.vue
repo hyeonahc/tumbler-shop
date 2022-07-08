36b5f021dbfbc8cabaa95bd7b4d7b67de9d29496
Kimchaewook <allurings2@naver.com>
2022-07-08 17:51:14 +0900

    feat: Add AllProductList, Computed, Created, Methods

--- src/routes/Admin.vue
+++ src/routes/Admin.vue
@@ -1,25 +1,64 @@
 <template>
   <h1>제품 조회</h1>
-  <router-link to="/AddProduct">
+  <router-link
+    to="/AddProduct">
     제품 추가
   </router-link>
 
   <div>
+    <button @click="allProductsLookup">
+      전체 제품 보기
+    </button>
+  </div>
+  <div>
     <table>
-      <tr>
-        <th>NO</th>
-        <th>Product Name</th>
-        <th>Product ID</th>
-        <th>Price</th>
-        <th>Status</th>
-      </tr>
+      <thead>
+        <tr>
+          <th>No</th>
+          <th>Product Name</th>
+          <th>Product ID</th>
+          <th>Price</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        <AllproductList 
+          v-for="product in allProducts"
+          :key="product.id"
+          :product="product"
+          :products="products" />
+      </tbody>
     </table>
   </div>
 </template>
 
 <script>
-export default {
 
+import AllproductList from '../components/AllproductList.vue'
+export default {
+  components: {
+    AllproductList
+  },
+
+  data() {
+    return {
+     id: ''
+    }
+  },
+
+  computed: {
+    allProducts() {
+      return this.$store.state.admin.allProducts
+    }
+  },
+  created() {
+    this.$store.dispatch('admin/allProductsLookup')
+  },
+  methods: {
+    async allProductsLookup() {
+      await this.$store.dispatch('admin/allProductsLookup')
+    },
+  }
 }
 </script>
 
