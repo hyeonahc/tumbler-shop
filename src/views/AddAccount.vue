677c3a72e18a2ded6633895072657a86061f3e5f
yueunhye <yulll035@naver.com>
2022-07-11 14:43:32 +0900

    feat: Add v-if,v-else... change route.query

--- src/views/AddAccount.vue
+++ src/views/AddAccount.vue
@@ -1,33 +1,52 @@
 <template>
-  <h2>{{ $route.params.bankname }} 계좌연결</h2>
-  <h2>{{ $route.params.bankcode }} </h2>
-  <h2>{{ $route.params.bankdigits }} </h2>
-  <!-- :placeholder="`계좌번호 ${addDigits()}자리 작성`" -->
+  <h2>$route.query.bankname: {{ $route.query.bankname }} 계좌연결</h2>
+  <h2>$route.query.bankcode: {{ $route.query.bankcode }} </h2>
+  <h2>$route.query.bankdigits: {{ $route.query.bankdigits }} </h2>
+  
+  
   <div>
-    계좌번호: <input
-      v-model="accountNumber"
-      type="text" />
-    <br />
-    핸드폰번호: <input
-      v-model.trim="phoneNumber"
-      type="text"
-      maxlength="11"
-      placeholder="핸드폰번호 '-' 없이입력" /><br />
-    계좌연결동의: <input
-      v-model="signature"
-      type="checkbox" /><br />
-    <button @click="$router.go(-1)">
-      뒤로가기
-    </button> 
-    <button @click="addBank()">
-      계좌등록
-    </button>
+    <div class="accountnumber">
+      <div>현재{{ accountNumber.length }}자리 입력</div>
+      계좌번호: <input
+        v-model="accountNumber"
+        type="text"
+        :placeholder="`계좌번호 ${addDigits()} '-' 없이입력`"
+        :maxlength="addDigits()"
+        oninput="this.value = this.value.replace(/[^0-9]/g, '')" />
+      <span>
+        <span v-if="accountNumber.length === addDigits()">작성 완료</span>
+        <span v-else> {{ addDigits() }}자리 입력해주세요</span>
+      </span>
+    </div>
+
+    <div class="phone-number">
+      핸드폰번호: <input
+        v-model="phoneNumber"
+        type="text"
+        placeholder="핸드폰번호 '-' 없이입력" 
+        maxlength="11"
+        oninput="this.value = this.value.replace(/[^0-9]/g, '')" />
+      <span>
+        <span v-if="phoneNumber.length === 11">작성 완료</span>
+        <span v-else> 전화번호 입력해주세요 </span>
+      </span>
+    </div>
+
+    <div class="signature">
+      계좌연결동의: <input
+        v-model="signature"
+        type="checkbox" />
+    </div>
+
+    <div class="button">
+      <button @click="$router.go(-1)">
+        뒤로가기
+      </button> 
+      <button @click="addBank()">
+        계좌등록
+      </button>
+    </div>
   </div>
-  이름:{{ hi }} 코드:{{ te }} 디짓트:{{ st }}
-  <button @click="ohoh">
-    aas
-  </button> 
-  <!-- {{ ohoh() }} -->
 </template>
 
 
@@ -35,6 +54,7 @@
 import { mapActions } from 'vuex'
 
 export default {
+
   data() {
     return {
       signature: '',
@@ -42,78 +62,36 @@ export default {
       phoneNumber: '',
     }
   },
-  //testcode
-  computed: {
-    hi() {
-      return 'getItem',window.localStorage.getItem('saveBankInfo')
-    },
-    te() {
-      return 'getItem',window.localStorage.getItem('test1')
-    },
-    st() {
-      return 'getItem',window.localStorage.getItem('test2')
-    }
-
-  },
-  //testcode
-  created() {
-    this.test()
-    
-  },
-
   mounted() {
     //params확인
-    console.log(this.$route.params)
-    this.ohoh()
+    console.log('params',this.$route)
   },
   methods: {
     ...mapActions('bank', [
       'accountConnect',
     ]),
-    // ohoh() {
-    //   const a = this.st
-    //   const b = a.map((i) => Number(i))
-    //   const c = b.reduce((x,y) => (x+y))
-    //   return c
-      
-      
-    // },
     //배열더하기
-    // addDigits() {
-    //   const bankdigits = this.$route.params.bankdigits
-    //   const digits = bankdigits.map((i) => Number(i))
-    //   const sum = digits.reduce((x,y) => (x+y))
-    //   return sum
-    // },
+    addDigits() {
+      const bankdigits = this.$route.query.bankdigits
+      const digits = bankdigits.map((i) => Number(i))
+      const sum = digits.reduce((x,y) => (x+y))
+      return sum
+    },
     //계좌연결
     addBank() {
+      if(this.accountNumber.length === this.addDigits() && this.phoneNumber.length === 11 && this.signature === true ) {
+        alert('계좌연동완료')
+        this.$router.go(-2)
+      }else{
+        return alert('유효한 값을 입력해주세요')
+      }
       this.accountConnect({
-        bankCode: this.$route.params.bankcode,
+        bankCode: this.$route.query.bankcode,
         accountNumber: this.accountNumber,
         phoneNumber: this.phoneNumber,
         signature: this.signature
       })
-      console.log('checkbox',this.signature)
-      console.log('bankCode',this.$route.params.bankcode)
-      alert('계좌연동완료')
-      this.$router.go(-2)
-    }, 
-    
-    //testcode
-    test() {
-      const savename = this.$route.params.bankname
-      const savecode = this.$route.params.bankcode
-      const savedigits = this.$route.params.bankdigits
-      window.localStorage.setItem('saveBankInfo', savename)
-      window.localStorage.setItem('test1', savecode)
-      window.localStorage.setItem('test2', savedigits)
-
-      console.log('kikiki',savename, savecode, savedigits)
-
-      
-    }
-    
-    
+    },
   }
 }
 </script>
