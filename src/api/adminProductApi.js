b10c6594b2c8b6b44393c5dfb16f87520c19f11c
Kimchaewook <allurings2@naver.com>
2022-07-11 16:08:44 +0900

    feat: Create adminProductApi.js

---
+++ src/api/adminProductApi.js
@@ -0,0 +1,28 @@
+export async function request(options) {
+  const {url = '', method, body } = options
+  const res = await fetch(`https://asia-northeast3-heropy-api.cloudfunctions.net/api/${url}`, {
+    method,
+    headers: {
+      'content-type': 'application/json',
+      'apikey': 'FcKdtJs202204',
+      'username': 'TEAM_1',
+      masterKey: true,
+    },
+    body: JSON.stringify(body)
+  })
+    return await res.json()
+}
+
+// 공용 관리자 API
+export async function commonRequst(id) {
+  const {url = '', method} = id
+  await fetch(`https://asia-northeast3-heropy-api.cloudfunctions.net/api/products/${url}`, {
+    method,
+    headers: {
+      'content-type': 'application/json',
+      'apikey': 'FcKdtJs202204',
+      'username': 'TEAM_1',
+      masterKey: true,
+    },
+   })
+}
