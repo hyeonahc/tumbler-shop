94acaa47991fbb37107fb08d3c3341ed8524579d
Kimchaewook <allurings2@naver.com>
2022-07-18 11:30:16 +0900

    feat: Add import, Div. Edit Img

--- src/components/AllproductList.vue
+++ src/components/AllproductList.vue
@@ -1,31 +1,26 @@
 <template>
   <tr>
     <td>
-      <div>{{ product.title }}</div>
-    </td>
-    <td>
-      <div>
-        {{ product.id }}
-      </div>
       <div>
         <img
           :src="
             product.thumbnail
               ? product.thumbnail
-              : 'https://via.placeholder.com/150'
+              : 'https://3.bp.blogspot.com/-ZKBbW7TmQD4/U6P_DTbE2MI/AAAAAAAADjg/wdhBRyLv5e8/s1600/noimg.gif'
           "
+          class=""
           alt="" />
+        {{ product.title }}
       </div>
     </td>
     <td>
       <div>
-        <div>{{ product.tags }}</div>
+        {{ product.id }}
       </div>
     </td>
     <td>
       <div>
-        <div>{{ product.price }} 원</div>
-        <div>{{ product.description }}</div>
+        <div>{{ product.price.toLocaleString('ko-KR') }} 원</div>
       </div>
     </td>
     <button @click="deleteProduct(product.id)">
@@ -33,7 +28,7 @@
     </button>
     <td>
       <router-link 
-        :to="{ name: 'EditProduct', params: { id: product.id }}">
+        :to="{ name: 'EditProduct', params: { id: product.id }, query: {title: product.title, price: product.price, thumbnail: product.thumbnail}}">
         상품 수정
       </router-link>
     </td>
@@ -41,7 +36,7 @@
 </template>
 
 <script>
-
+import { request } from '../api/adminProductApi.js'
 export default { 
   props: {
     product: {
@@ -50,11 +45,19 @@ export default {
     }
   },
   methods: {
-    async deleteProduct(id) {
-      await this.$store.dispatch('admin/deleteProduct', {
-        id : id
+    // async deleteProduct(id) {
+    //   await this.$store.dispatch('admin/deleteProduct', {
+    //     id : id
+    //   })
+    // },
+    async deleteProduct() {
+      const res = await request({
+        url: `${this.product.id}`,
+        method: 'DELETE'
       })
-    },
+      console.log(res)
+      this.product
+    }
   }
 }
 
