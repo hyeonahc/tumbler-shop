0fea5b38392d72eade94236c31014fffa431b91a
heeyoung-c <heeyoungchun.dev@gmail.com>
2022-07-18 21:43:39 +0900

    feat: Create SalesGraph.vue

---
+++ src/components/SalesGraph.vue
@@ -0,0 +1,33 @@
+<template>
+  <h1>매출 그래프</h1>
+  <button @click="useInqureyProducts">
+    조회
+  </button>
+  <ul
+    v-for="saleItem in salesGraph"
+    :key="saleItem.detaildId">
+    {{ saleItem.timePaid }}
+    {{ saleItem.product.price }}
+  </ul>
+</template>
+
+<script>
+import { mapState, mapActions } from 'vuex'
+
+export default {
+  computed: {
+    ...mapState('admin', [
+      'salesGraph'
+    ])
+  },
+  methods: {
+    ...mapActions('admin', [
+      'inqureyProducts'
+    ]),
+    useInqureyProducts() {
+      this.inqureyProducts()
+    }
+  },
+  
+}
+</script>
