d4e07c55a955e656b047f65a188aa6849481686a
Kimchaewook <allurings2@naver.com>
2022-07-18 17:40:37 +0900

    feat: Delete DeleteProduct, Edit Request -> PublicRequest

--- src/store/admin.js
+++ src/store/admin.js
@@ -1,4 +1,5 @@
-import {request} from '../api/adminProductApi'
+// import { request } from '../api/adminProductApi'
+import { publicRequest } from '../api/publicRequest'
 
 
 export default {
@@ -30,7 +31,8 @@ export default {
   actions: {
     // 모든 제품 조회
     async allProductsLookup({ commit }) {
-      const res = await request({
+      const res = await publicRequest({
+        url: 'products',
         method: 'GET',
       })
         commit('assignState',{allProducts: res})
@@ -39,30 +41,27 @@ export default {
 
     // 제품 추가
     async addProduct({ commit }, payload = {}) {
-      const res = await request({
+      const {title, price, description, tags, thumbnailBase64} = payload
+      const res = await publicRequest({
+        url: 'products',
         method: 'POST',
         body: {
-        ...payload
+          title,
+          price: Number(price),
+          description,
+          tags,
+          thumbnailBase64
         }
       })
       commit('assignState', {allProducts: res})
       console.log('제품 추가')
-    },
-
-    // 제품 삭제
-    async deleteProduct({ commit },id) {
-      const res = await request({
-        url:`${id.id}`,
-        method: 'DELETE',
-      })
-        commit('delete', res)
-        console.log('제품 삭제')
+      console.log(res)
     },
 
     // 전체 판매 내역
     async salesProduct({ commit }) {
-      const res = await request({
-        url:'transactions/all',
+      const res = await publicRequest({
+        url:'products/transactions/all',
         method: 'GET'
       })
         commit('assignState', {salesDetails: res})
