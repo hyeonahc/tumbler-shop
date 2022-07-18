b15513781a622af0db18a5db25f672d9c5468c41
yueunhye <yulll035@naver.com>
2022-07-19 03:34:20 +0900

    feat: Add table & v-for 'index' props

--- src/components/AccountList.vue
+++ src/components/AccountList.vue
@@ -1,46 +1,76 @@
 <template>
   <div class="account-list-container">
     <div v-if="isOn">
-      <h4>계좌목록</h4>   
-      <div
-        v-for="bank in banklist"
-        :key="bank.code"
-        class="banklist">
-        <div v-show="!bank.disabled">
-          <button @click="addAccount(bank)">
+      <h3>결제 계좌 등록</h3>   
+      <ul>
+        <li
+          v-for="bank in banklist"
+          :key="bank.code"
+          class="banklist">
+          <div v-show="!bank.disabled">
+            <img
+              :key="bank.image"
+              :src="bank.image"
+              alt="image"
+              @click="addAccount(bank)" />
             {{ bank.name }}
-          </button> 
-        </div>
-      </div>
+          </div>
+        </li>
+      </ul> 
+      <button @click="addAccountNext">
+        확인
+      </button>
     </div>
-    <div v-else>
-      <h1>{{ selectedBank.name }} 계좌 등록</h1>
+    <div
+      v-else
+      class="register">
+      <h3>{{ selectedBank.name }} 계좌 등록</h3>
       <div>
-        <label for="account-number">계좌번호</label>
+        <label for="account-number">계좌번호</label><br />
         <input
           id="account-number"
           v-model="accountNumber"
           :placeholder="`계좌번호 ${selectedBankDigitsLength}자리를 - 없이 입력하세요`"
           :maxlength="selectedBankDigitsLength"
           type="text" />
+        <h6
+          v-if="accountNumber.length === selectedBankDigitsLength "
+          class="success">
+          {{ accountNumber.length }} 자리 입력 완료
+        </h6>
+        <h6
+          v-else
+          class="red">
+          {{ selectedBankDigitsLength }} 자리를 입력해주세요
+        </h6>
       </div>
       <div>
-        <label for="phone-number">핸드폰 번호</label>
+        <label for="phone-number">핸드폰 번호</label><br />
         <input
           id="phone-number"
           v-model="phoneNumber"
           :placeholder="`핸드폰 번호 11자리를 - 없이 입력하세요`"
           :maxlength="11"
           type="text" />
+        <h6
+          v-if="phoneNumber.length === 11"
+          class="success">
+          {{ phoneNumber.length }} 자리 입력 완료
+        </h6>
+        <h6
+          v-else
+          class="red">
+          11 자리를 입력해주세요
+        </h6>
       </div>
       <div>
-        <label for="signature">
-          계좌연결동의
-        </label>
         <input
           id="signature"
           v-model="signature"
           type="checkbox" />
+        <label for="signature">
+          계좌연결동의
+        </label>
       </div>
       <button @click="connectAccount()">
         계좌등록
@@ -60,19 +90,19 @@ export default {
       selectedBankDigitsLength: '',
       accountNumber: '',
       phoneNumber: '',
-      signature: false
+      signature: false,
+      
     }
   },
   computed: {
   ...mapState('bank', [
       'banklist',
-      'userAccountInfo'
+      'userAccountInfo',
     ]),
   },
   created() {
     this.accountList()
     this.accountBalance()
-    console.log('this.banklist', this.banklist)
   },
   methods: {
     ...mapActions('bank', [
@@ -83,7 +113,11 @@ export default {
     addAccount(bank) {
       console.log(bank)
       this.selectedBank = bank
-      console.log(this.selectedBank)
+      // console.log(this.selectedBank)
+      // this.isOn = false
+      // this.createDigitsLength()
+    },
+    addAccountNext() {
       this.isOn = false
       this.createDigitsLength()
     },
@@ -111,11 +145,45 @@ export default {
         return alert('유효한 값을 입력해주세요')
       }
     },
+
   },
 }
 </script>
 
 <style lang="scss" scoped>
+div {
+  // align-items: center;
+}
+label {
+  // border: 1px solid black;
+  // padding: 5px;
+}
+input {
+
+  margin-top: 5px;
+}
+  #account-number, #phone-number {
+    width: 100%;
+  }
+
+input:focus {
+  outline-color: $color-primary;
+}
+h6 {
+  margin: 7px;
+  
+}
+button {
+  margin-top: 30px;
+  background-color: $color-primary;
+  color: $color-white;
+  border: none;
+  border-radius: 3px;
+  padding: 5px;
+  display: block;
+  
+  
+}
 .account-list-container {
   position: absolute;
   top: 50%;
@@ -124,14 +192,36 @@ export default {
   background-color: white;
   border-radius: 5px;
   padding: 5em;
-  .banklist {
-    color: $color-font;
-    font-size: 2rem;
-    font-weight: 700;
-    text-align: center;
+  width: 40%;
+  height: 50%;
+  h3 {
+      color: $color-font;
+      font-size: 2rem;
+      font-weight: 700;
+      text-align: center;
+    }
+  ul {
+    display: flex;
+    flex-wrap: wrap;
+    border: 1px solid $color-font;
+    li {
+      margin: 5px;
+      padding: 5px;
+      
+      // .banklist {
+      //   color: $color-font;
+      //   font-size: 2rem;
+      //   font-weight: 700;
+      //   text-align: center;
+      // }
+    }
   }
+  
   .success {
     color: $color-primary;
+  }
+  .red {
+    color: $color-warning;
   }
 }
 </style>
