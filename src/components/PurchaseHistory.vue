d1f1358797572ce6d9e58b5a6c7e860a38373779
hyeonah <hyeonah.hello@gmail.com>
2022-07-18 16:49:01 +0900

    feat: Add search feature, change variable name and remove unnecessary console code for testing

--- src/components/PurchaseHistory.vue
+++ src/components/PurchaseHistory.vue
@@ -4,7 +4,8 @@
     <div>
       <input
         type="text"
-        placeholder="상품을 검색하세요" />
+        placeholder="상품을 검색하세요"
+        @input="search = $event.target.value" />
     </div>
     <table>
       <tr>
@@ -17,34 +18,34 @@
         <th>구매상태</th>
       </tr>
       <tr
-        v-for="purchasedProduct in purchasedProducts"
-        :key="purchasedProduct.id">
-        <td>{{ purchasedProducts.indexOf(purchasedProduct) + 1 }}</td>
-        <td>{{ purchasedProduct.title }}</td>
-        <td>{{ purchasedProduct.price }} 원</td>
-        <td>{{ purchasedProduct.time }}</td>
+        v-for="product in searchedProducts"
+        :key="product.id">
+        <td>{{ searchedProducts.indexOf(product) + 1 }}</td>
+        <td>{{ product.title }}</td>
+        <td>{{ product.price }} 원</td>
+        <td>{{ product.time }}</td>
         <td>
           <span
-            v-show="purchasedProduct.status === '구매신청'"
+            v-show="product.status === '구매신청'"
             class="confirm-btn"
-            @click="confirmPurchase(purchasedProduct)">구매확정</span>
+            @click="confirmPurchase(product)">구매확정</span>
         </td>
         <td>
           <span
-            v-show="purchasedProduct.status === '구매신청'"
+            v-show="product.status === '구매신청'"
             class="cancel-btn"
-            @click="cancelPurchase(purchasedProduct)">구매취소</span>
+            @click="cancelPurchase(product)">구매취소</span>
         </td>
         <td>
           <span
-            v-if="purchasedProduct.status === '구매신청'"
-            class="status-badge request-badge">{{ purchasedProduct.status }}</span>
+            v-if="product.status === '구매신청'"
+            class="status-badge request-badge">{{ product.status }}</span>
           <span
-            v-else-if="purchasedProduct.status === '구매확정'"
-            class="status-badge confirm-badge">{{ purchasedProduct.status }}</span>
+            v-else-if="product.status === '구매확정'"
+            class="status-badge confirm-badge">{{ product.status }}</span>
           <span
             v-else
-            class="status-badge cancel-badge">{{ purchasedProduct.status }}</span>
+            class="status-badge cancel-badge">{{ product.status }}</span>
         </td>
       </tr>
     </table>
@@ -57,12 +58,15 @@ import { publicRequest } from '../api/publicRequest'
 export default {
 	data() {
 		return {
+			search: '',
 			purchasedProducts: [],
 		}
 	},
-	watch: {
-		purchasedProducts(value) {
-			console.log(value)
+	computed: {
+		searchedProducts() {
+			return this.purchasedProducts.filter(product => {
+				return product.title.match(this.search)
+			})
 		}
 	},
 	created() {
@@ -74,12 +78,10 @@ export default {
 				url: 'products/transactions/details',
 				method: 'GET',
 			})
-			// console.log('res: ', res)
 			this.createPurchasedProducts(res)
 		},
 		createPurchasedProducts(res) {
 			const purchaseHistoryLists = res.sort((a, b) => new Date(a.timePaid) - new Date(b.timePaid))
-			// console.log('purchaseHistoryLists: ', purchaseHistoryLists)
 			purchaseHistoryLists.forEach(purchaseHistoryList => {
 				let { product: {title, price}, timePaid, done, isCanceled, detailId } = purchaseHistoryList
 				price = price.toLocaleString('ko-KR')
@@ -100,9 +102,7 @@ export default {
 					status: status,
 					id: detailId
 				}
-				// console.log(info)
 				this.purchasedProducts.push(info)
-				// console.log(this.purchasedProducts)
 			})
 		},
 		async confirmPurchase(purchasedProduct) {
@@ -114,7 +114,6 @@ export default {
 				method: 'POST',
 				body: body
 			})
-			console.log(res)
 			if(res === true) {
 				alert('구매가 확정되었습니다.')
 				this.$router.go()
@@ -131,7 +130,6 @@ export default {
 				method: 'POST',
 				body: body
 			})
-			console.log(res)
 			if(res === true) {
 				alert('구매가 취소되었습니다.')
 				this.$router.go()
