81a1583a00eb6466afffb956fbd191c80ffe909b
hyeonah <hyeonah.hello@gmail.com>
2022-07-19 14:05:24 +0900

    feat: Undo separating table component for pagination feature (This feature will be developed later)

--- src/components/PurchaseHistory.vue
+++ src/components/PurchaseHistory.vue
@@ -4,32 +4,65 @@
     <div>
       <input
         type="text"
-        placeholder="상품을 검색하세요"
-        @input="search = $event.target.value" />
+        placeholder="상품을 검색하세요" />
     </div>
-    <PurchaseHistoryTable :searched-products="searchedProducts" />
+    <table>
+      <tr>
+        <th>번호</th>
+        <th>상품명</th>
+        <th>상품가격</th>
+        <th>주문일</th>
+        <th>구매확정</th>
+        <th>구매취소</th>
+        <th>구매상태</th>
+      </tr>
+      <tr
+        v-for="purchasedProduct in purchasedProducts"
+        :key="purchasedProduct.id">
+        <td>{{ purchasedProducts.indexOf(purchasedProduct) + 1 }}</td>
+        <td>{{ purchasedProduct.title }}</td>
+        <td>{{ purchasedProduct.price }} 원</td>
+        <td>{{ purchasedProduct.time }}</td>
+        <td>
+          <span
+            v-show="purchasedProduct.status === '구매신청'"
+            class="confirm-btn"
+            @click="confirmPurchase(purchasedProduct)">구매확정</span>
+        </td>
+        <td>
+          <span
+            v-show="purchasedProduct.status === '구매신청'"
+            class="cancel-btn"
+            @click="cancelPurchase(purchasedProduct)">구매취소</span>
+        </td>
+        <td>
+          <span
+            v-if="purchasedProduct.status === '구매신청'"
+            class="status-badge request-badge">{{ purchasedProduct.status }}</span>
+          <span
+            v-else-if="purchasedProduct.status === '구매확정'"
+            class="status-badge confirm-badge">{{ purchasedProduct.status }}</span>
+          <span
+            v-else
+            class="status-badge cancel-badge">{{ purchasedProduct.status }}</span>
+        </td>
+      </tr>
+    </table>
   </div>
 </template>
 
 <script>
-import PurchaseHistoryTable from './PurchaseHistoryTable.vue'
 import { publicRequest } from '../api/publicRequest'
 
 export default {
-	components: {
-		PurchaseHistoryTable
-	},
 	data() {
 		return {
-			search: '',
 			purchasedProducts: [],
 		}
 	},
-	computed: {
-		searchedProducts() {
-			return this.purchasedProducts.filter(product => {
-				return product.title.match(this.search)
-			})
+	watch: {
+		purchasedProducts(value) {
+			console.log(value)
 		}
 	},
 	created() {
@@ -41,10 +74,11 @@ export default {
 				url: 'products/transactions/details',
 				method: 'GET',
 			})
+			// console.log('res: ', res)
 			this.createPurchasedProducts(res)
 		},
-		createPurchasedProducts(res) {
-			const purchaseHistoryLists = res.sort((a, b) => new Date(a.timePaid) - new Date(b.timePaid))
+		createPurchasedProducts(purchaseHistoryLists) {
+			// console.log('purchaseHistoryLists: ', purchaseHistoryLists)
 			purchaseHistoryLists.forEach(purchaseHistoryList => {
 				let { product: {title, price}, timePaid, done, isCanceled, detailId } = purchaseHistoryList
 				price = price.toLocaleString('ko-KR')
@@ -65,9 +99,106 @@ export default {
 					status: status,
 					id: detailId
 				}
+				// console.log(info)
 				this.purchasedProducts.push(info)
+				// console.log(this.purchasedProducts)
 			})
 		},
+		async confirmPurchase(purchasedProduct) {
+			const body = {
+				detailId: purchasedProduct.id
+			}
+			const res = await publicRequest({
+				url: 'products/ok',
+				method: 'POST',
+				body: body
+			})
+			console.log(res)
+			if(res === true) {
+				alert('구매가 확정되었습니다.')
+				this.$router.go()
+			} else {
+				alert(res.error)
+			}
+		},
+		async cancelPurchase(purchasedProduct) {
+			const body = {
+				detailId: purchasedProduct.id
+			}
+			const res = await publicRequest({
+				url: 'products/cancel',
+				method: 'POST',
+				body: body
+			})
+			console.log(res)
+			if(res === true) {
+				alert('구매가 취소되었습니다.')
+				this.$router.go()
+			} else {
+				alert(res.error)
+			}
+		}
 	}
 }
 </script>
+
+<style lang="scss" scoped>
+table {
+  border-collapse: collapse;
+  width: 900px;
+	font-size: 16px;
+
+	tr {
+		&:nth-child(even){
+			background-color: #f2f2f2;
+		}
+		&:hover {
+			background-color: #ddd;
+		}
+	}
+
+	th {
+		padding-top: 12px;
+		padding-bottom: 12px;
+		text-align: left;
+		background-color: #2C6634;
+		color: white;
+	}
+
+	th, td {
+		border: 1px solid #ddd;
+    padding: 10px;
+	}
+
+	.confirm-btn, .cancel-btn {
+		text-decoration: underline;
+		cursor: pointer
+	}
+
+	.confirm-btn {
+		color: #2C6634
+	}
+
+	.cancel-btn {
+		color: #FF6347
+	}
+
+	.status-badge {
+		padding: 5px 10px;
+		border-radius: 25px;
+		&.request-badge {
+			background-color: #FEF7E6;
+			color: #FAAE00;
+		}
+
+		&.confirm-badge {
+			background-color: #F4F9F3;
+			color: #2C6634;
+		}
+		&.cancel-badge {
+			background-color: #FFEFEC;
+			color: #FF6347;
+		}
+	}
+}
+</style>
