08160f3c61e6bc20e5ae6a5b8beaf2a227d0cbec
yueunhye <yulll035@naver.com>
2022-07-20 16:48:13 +0900

    feat: Add object banklist.image

--- src/store/bank.js
+++ src/store/bank.js
@@ -23,13 +23,13 @@ export default {
         method: 'GET',
       })
       state.banklist = res
-      state.banklist[0].image= require('../assets/logo.png')
-      state.banklist[1].image= require('../assets/logo.png')
-      state.banklist[2].image= require('../assets/logo.png')
-      state.banklist[3].image= require('../assets/logo.png')
-      state.banklist[4].image= require('../assets/logo.png')
-      state.banklist[5].image= require('../assets/logo.png')
-      state.banklist[6].image= require('../assets/logo.png')
+      state.banklist[0].image = require('../assets/bank_kb.png')
+      state.banklist[1].image= require('../assets/bank_shinhan.png')
+      state.banklist[2].image= require('../assets/bank_woori.png')
+      state.banklist[3].image= require('../assets/bank_hana.png')
+      state.banklist[4].image= require('../assets/bank_kbank.png')
+      state.banklist[5].image= require('../assets/bank_kakao.png')
+      state.banklist[6].image= require('../assets/bank_nh.png')
       console.log('res', state.banklist)
     },
     // 계좌 연결
