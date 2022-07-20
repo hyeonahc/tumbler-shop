2b1a0431cbdb3b08fe010f021e323119388c8f8d
heeyoung-c <heeyoungchun.dev@gmail.com>
2022-07-20 13:23:14 +0900

    move: Move code to `menu.js` store

--- src/store/menu.js
+++ src/store/menu.js
@@ -5,17 +5,27 @@ export default {
       { name: '구매내역', isShow: true },
       { name: '내 계좌', isShow: false},
       { name: '내 정보 수정', isShow: false}
+    ],
+    adminPageMenuList: [
+      { name: '제품 조회', isShow: true },
+      { name: '판매 내역', isShow: false },
+      { name: '판매 현황', isShow: false } 
     ]
   }),
   mutations: {
     updateMenu(state, menuName) {
-      state.myPageMenuList.forEach(menu => {
+      const whatMenu = 
+      state.myPageMenuList.some(menu => menu.name === menuName)
+      ? state.myPageMenuList 
+      : state.adminPageMenuList
+      
+      whatMenu.forEach(menu => {
         menu.isShow = false
         if (menu.name === menuName) {
           menu.isShow = true
         }
       })
-    },
+    }
   },
   actions: {
 		isShowMenu({ commit }, menuName) {
