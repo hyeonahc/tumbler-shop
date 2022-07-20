ec23263154c8722064ab49646b0a286867bae759
heeyoung-c <heeyoungchun.dev@gmail.com>
2022-07-20 19:02:38 +0900

    test: Add Code for testing serverless functions

--- functions/publicRequest.js
+++ functions/publicRequest.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const handler = async event => {
   const { url = '', method, accessToken = '', data } = JSON.parse(event.body) // `event.body`는 문자이므로 `JSON.parse` 를 해준다
-  await axios({
+  const { data: returnValue } = await axios({
     url: `https://asia-northeast3-heropy-api.cloudfunctions.net/api/${url}`,
     method,
     headers: {
@@ -14,5 +14,10 @@ const handler = async event => {
     },
     data
   })
+  return {
+    statusCode: 200,
+    body: JSON.stringify(returnValue)
+  }
 }
 export { handler }
+
