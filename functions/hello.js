0f0254135e70870bdcdea7c9b94959f072443fff
heeyoung-c <heeyoungchun.dev@gmail.com>
2022-07-21 15:21:43 +0900

    Merge branch 'Heeyoung' into main & conf: Remove netlify conf

--- functions/hello.js
+++
@@ -1,9 +0,0 @@
-exports.handler = async function (event, context) {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      name: 'Hee',
-      age: '85'
-    }) // 문자데이터만 할당이 가능, 객체를 넣기 위해 JSON.stringfy 사용
-  }
-}
--- src/api/publicRequest.js
+++ src/api/publicRequest.js
@@ -1,4 +1,4 @@
-// 공용 요청 원래 버전
+// 공용 요청
 // Public Request function
 export async function publicRequest(options) {
   const accessToken = window.localStorage.getItem('token')
@@ -16,21 +16,3 @@ export async function publicRequest(options) {
   })
   return res.json()
 }
-
-// 공용 요청 서버리스 버전
-// Public Request function
-// export async function publicRequest(options) {
-//   const accessToken = window.localStorage.getItem('token')
-//   const { url, method, body: data } = options
-//   // 도메인/.netlify/functions/publicRequest // 여기서 도메인 부분은 생략 가능
-//   const res = await fetch('.netlify/functions/publicRequest', {
-//     method: 'POST', // 서버로 날리는 요청이므로 `POST`로 날려야 `body`가 들어간다! 서버로 전송하는 코드는 `POST`
-//     body: JSON.stringify({
-//       url,
-//       method,
-//       accessToken,
-//       data
-//     })
-//   })
-//   return res.json()
-// }
