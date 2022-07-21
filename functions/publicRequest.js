9a80a2ae5cdeae2f230ca53099d7c979e2a3d9e8
heeyoung-c <heeyoungchun.dev@gmail.com>
2022-07-21 23:06:28 +0900

    fix: Fix serverless functions

--- functions/publicRequest.js
+++ functions/publicRequest.js
@@ -1,9 +1,7 @@
 const axios = require('axios')
 
 exports.handler = async function handler(event) {
-  console.log('요청 들어옴')
   const { url, method, accessToken, data } = JSON.parse(event.body) // `event.body`는 문자이므로 `JSON.parse` 를 해준다
-
   try {
     const { data: returnValue } = await axios({
       url: `https://asia-northeast3-heropy-api.cloudfunctions.net/api/${url}`,
@@ -17,8 +15,6 @@ exports.handler = async function handler(event) {
       },
       data
     })
-    console.log(returnValue)
-    console.log('일단 성공')
     if (returnValue.Error) {
       return {
         statusCode: 400,
@@ -30,8 +26,6 @@ exports.handler = async function handler(event) {
       body: JSON.stringify(returnValue)
     }
   } catch (error) {
-    console.log(error)
-    console.log('에러')
     return {
       statusCode: error.response.status,
       body: error.message
