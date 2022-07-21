2ee049d65d497268d2cec73dc9be567e5bea9cea
Kimchaewook <allurings2@naver.com>
2022-07-21 22:23:40 +0900

    style: Add class name and Make layout

--- src/components/SingleProductLookup.vue
+++ src/components/SingleProductLookup.vue
@@ -1,40 +1,42 @@
 <template>
-  <div>
-    <div>
-      <i
-        class="fa-solid fa-xmark"
-        @click="a"></i>
-    </div>
-    <div>
+  <div class="single-product__wrap">
+    <div class="single-product__wrap--img flex-center-vertically">
       <img
         :src="product.thumbnail"
         :alt="product.title" />
-      <div>
-        {{ product.title }}
+    </div>
+    <div class="single-product__wrapdata">
+      <div class="single-product__wrapdata--title">
+        <h1>{{ product.title }}</h1>
       </div>
-      <div>
-        {{ product.price.toLocaleString('ko-KR') }}원
+      <div class="single-product__wrapdata--price">
+        <h3>{{ product.price.toLocaleString('ko-KR') }}원</h3>
       </div>
-      <div>
+      <div class="single-product__wrapdata--description">
         {{ product.description }}
       </div>
-    </div>
-    <button
-      @click="test">
-      제품 수정
-    </button>
-    <div
-      v-if="modal"
-      class="black">
-      <div class="white">
-        <EditProduct
-          :product="product" />
+      <div class="single-product__wrapdata--button">
+        <button
+          class="status-badge confirm-badge"
+          @click="test">
+          제품 수정
+        </button>
+        <div
+          v-if="modal"
+          class="black">
+          <div class="white">
+            <EditProduct
+              :product="product" />
+          </div>
+        </div>
+        <div>
+          <button 
+            class="status-badge cancel-badge"
+            @click="deleteProduct(product.id)">
+            제품 삭제
+          </button>
+        </div>
       </div>
-    </div>
-    <div>
-      <button @click="deleteProduct(product.id)">
-        제품 삭제
-      </button>
     </div>
   </div>
 </template>
@@ -92,12 +94,53 @@ export default {
 
   
 <style lang="scss" scoped>
-  .fa-solid {
-    font-size: 20px;
+  .single-product {
+    &__wrap {
+      display: flex;
+      align-content: center;
+      height: 100%;
+      &--img {
+        img {
+          height: 30vh;
+        }
+      }
+    }
+    &__wrapdata {
+      width: 100%;
+      margin: auto;
+      &--title {
+        h1 {
+          margin: 0;
+          padding-bottom: 15px;
+          border-bottom: 1px solid;
+        }
+      }
+      &--price {
+        padding: 15px 0;
+        color: $color-primary;
+      }
+      &--description {
+        padding: 5px 0;
+      }
+      &--button {
+        display: flex;
+        margin-top: 3vh;
+        .confirm-badge{
+          background-color: $color-primary;
+          color: $color-primary-light;
+        }
+        .cancel-badge {
+          margin-left: 10px;
+          background-color: $color-danger;
+          color:  $color-danger-light;
+        }
+      }
+    }
+
   }
   .white {
-    width: 60vw;
-    height: 70vh;
+    width: 50vw;
+    height: 64vh;
     background-color: white;
     position: absolute;
     top: 50%;
