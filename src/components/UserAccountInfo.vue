ea65c13926a8bb3eb35b40f1ce93b13f1d7deb8a
yueunhye <yulll035@naver.com>
2022-07-21 15:55:19 +0900

    style: Add style

--- src/components/UserAccountInfo.vue
+++ src/components/UserAccountInfo.vue
@@ -1,28 +1,25 @@
 <template>
-  <!-- {{ userInfo }} -->
   <tr>
     <td>{{ index + 1 }}</td>
     <td>{{ userInfo.bankName }}</td>
     <td>{{ userInfo.accountNumber }}</td>
     <td>{{ userInfo.balance.toLocaleString('ko-KR') }} 원</td>
     <td
-      class="remove-account"
+      class="action-btn cancel-btn"
       @click="isShow = true">
       계좌해지
     </td>
   </tr>
-  
-  
-  
   <div
     v-if="isShow == true"
     class="modal">
     <div
-      class="modal_content">
+      class="modal_content flex-center-center">
       <h1>{{ userInfo.bankName }}</h1> 
       <h2>{{ userInfo.accountNumber }}</h2> 
       <h3>{{ userInfo.balance.toLocaleString('ko-KR') }} 원</h3> 
-      <h4>해당계좌를 정말로 해지하시겠습니까? <br />해지시 되돌릴 수 없습니다</h4>
+      <h4>해당계좌를 정말로 해지하시겠습니까? </h4>
+      <h4>해지시 되돌릴 수 없습니다</h4>
       <div class="btn">
         <button
           class="agree"
@@ -43,7 +40,6 @@
 import { mapActions } from 'vuex'
 
 export default {
-  // myAccount에서 userAccountInfo props
   props: {
       userInfo: {
         type: Object,
@@ -80,65 +76,36 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-h1,h2,h3 {
-  padding: 15px;
-  color: $color-font;
-  font-weight: 700;
-}
-h4 {
-  color: $color-warning;
-  padding-top: 20px;
-  padding-bottom: 20px;
-  line-height: 30px;
-}
-td {
-  padding: 20px;
-  
-}
-.remove-account {
-    color: $color-warning;
-    cursor: pointer;
-    text-decoration: underline;
-}
-div {
-  box-sizing: border-box;
-}
-.myaccount {
-  border-bottom: 1px dotted black
-}
 .modal {
   height: 100vh;
   width: 100vw;
   position: fixed;
   left: 0;
   top: 0;
-  background: rgba(0,0,0,0.5);
+  background: $color-bg-filter;
   .modal_content {
     position: absolute;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
-    background-color: white;
-    border-radius: 5px;
+    background-color: $color-white;
+    border-radius: .5rem;
     padding: 2em;
-    width: 30%;
-    height: 50%;
-    .btn {
-      display: flex;
-      justify-content: space-evenly;
-      button {
-        cursor: pointer;
-        background-color: $color-primary;
-        color: $color-white;
-        width: 20%;
-        padding: 1.2rem;
-        border: none;
-        border-radius: 3px;
-        }
+    width: 45rem;
+    height: 50rem;
+    flex-wrap: wrap;
+    text-align: center;
+    h1, h2, h3, h4 {
+      width: 100%;
     }
-  }
-    
+    h4 {
+      color: $color-danger;
+    }
+    .btn {
+      width: 20rem;
+      display: flex;
+      justify-content: space-between;
+    }
+  }   
 }
-
-  
 </style>
